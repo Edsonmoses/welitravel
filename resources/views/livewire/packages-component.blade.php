6437dba13ff6ba8b61711448a2b3db5bf260b539
Edson Wandera <edsonwandera@yahoo.com>
2022-07-25 10:19:37 +0300

    packages

--- resources/views/livewire/packages-component.blade.php
+++ resources/views/livewire/packages-component.blade.php
@@ -1,3 +1,48 @@
 <div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+  <div class="content-body">
+      <!-- page section about-->
+      <section class="page-section pt-110 pb-110">
+        <div class="container">
+          <div class="row"> 
+            <div class="col-md-6"><img src="{{ asset('assets/frontend/pic/packages/August-Umrah.jpg')}}" data-at2x="{{ asset('assets/frontend/pic/packages/August-Umrah.jpg')}}" alt class="mt-minus-20"></div>
+            <div class="col-md-6">
+              <!-- section title-->
+              <h2 class="title-section mt-0 mb-0">August Umrah</h2>
+              <!-- ! section title-->
+              <div class="cws_divider with-plus short-3 mb-20 mt-10"></div>
+              <p class="mb-20">Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, port titor eu, consequat vitae, eleifend ac, enim.</p>
+              <p class="mb-20">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis <ins>natoque penatibus</ins> et magnis dis parturient montes, nascetur ridi culus mus. Donec quam felis, ultricies nec, pellentesque eu :</p>
+              <!-- list-->
+              <ul class="style-2 mb-30">
+                <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit seres</li>
+                <li>Donec pede justo, fringilla vel, aliquet nec, vulputate eget</li>
+                <li>In enim justo, rhoncus ut, imperdiet a, venenatis vitae, jur</li>
+                <li>Aenean leo ligula, porttitor eu, consequat vitae, eleifend	</li>
+              </ul>
+              <!-- ! list--><a href="#" class="cws-button alt">Book Now</a><!--<a href="#" class="cws-button alt gray-dark">Learn more</a>-->
+            </div>
+          </div>
+          <div class="row pt-30"> 
+            <div class="col-md-6"><img src="{{ asset('assets/frontend/pic/packages/September-Umrah.jpg')}}" data-at2x="{{ asset('assets/frontend/pic/packages/September-Umrah.jpg')}}" alt class="mt-minus-20"></div>
+            <div class="col-md-6">
+              <!-- section title-->
+              <h2 class="title-section mt-0 mb-0">September Umrah</h2>
+              <!-- ! section title-->
+              <div class="cws_divider with-plus short-3 mb-20 mt-10" style="border-bottom: 2px solid #832ca7 !important"></div>
+              <p class="mb-20">Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, port titor eu, consequat vitae, eleifend ac, enim.</p>
+              <p class="mb-20">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis <ins>natoque penatibus</ins> et magnis dis parturient montes, nascetur ridi culus mus. Donec quam felis, ultricies nec, pellentesque eu :</p>
+              <!-- list-->
+              <ul class="style-2 mb-30">
+                <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit seres</li>
+                <li>Donec pede justo, fringilla vel, aliquet nec, vulputate eget</li>
+                <li>In enim justo, rhoncus ut, imperdiet a, venenatis vitae, jur</li>
+                <li>Aenean leo ligula, porttitor eu, consequat vitae, eleifend	</li>
+              </ul>
+              <!-- ! list--><a href="#" class="cws-button alt">Book Now</a><!--<a href="#" class="cws-button alt gray-dark">Learn more</a>-->
+            </div>
+          </div>
+        </div>
+        
+      </section>
+      <!-- ! page section about-->
 </div>
