b959464861586020b0a2bbfc3d122248e442c228
Edson Wandera <edsonwandera@yahoo.com>
2022-07-27 15:53:09 +0300

    gallery changes

--- resources/views/livewire/gallery-tours-component.blade.php
+++ resources/views/livewire/gallery-tours-component.blade.php
@@ -2,94 +2,126 @@
     <div class="container page purl">
         <div class="work-filter menu-widget with-switch">
           <ul class="magic-line">
-            <!-- <li class="current_item"><a href="#" data-filter="*" class="filter active">All</a></li>
-           <li><a href="#" data-filter=".family" class="filter">Family</a></li>
-            <li><a href="#" data-filter=".adventure" class="filter">Adventure</a></li>
-            <li><a href="#" data-filter=".romantic" class="filter">Romantic</a></li>
-            <li><a href="#" data-filter=".wildlife" class="filter">Wildlife</a></li>
-            <li><a href="#" data-filter=".beach" class="filter">Beach</a></li>
+            <li class="current_item"><a href="#" data-filter="*" class="filter active">All</a></li>
+            <li><a href="#" data-filter=".adventure" class="filter">Historical adventure</a></li>
+            <li><a href="#" data-filter=".adventure" class="filter btn disabled" ></a></li>
+            <li><a href="#" data-filter=".romantic" class="filter btn disabled"></a></li>
+            <li><a href="#" data-filter="." class="filter btn disabled"></a></li>
+            <!--<li><a href="#" data-filter=".beach" class="filter">Beach</a></li>
             <li><a href="#" data-filter=".honeymoon" class="filter">Honeymoon</a></li>-->
           </ul>
         </div>
         <div id="filter-grid" class="row portfolio-grid gallery">
-          {{-- <!-- portfolio item-->
-          <div class="col-md-6 col-sm-12 col-xs-12 all adventure wildlife beach">
+          <!-- portfolio item-->
+          <div class="col-md-6 col-sm-12 col-xs-12 all">
             <div class="portfolio-item big">
-              <!-- portfolio image--><a href="{{ asset('assets/frontend/pic/portfolio/1160x570_Portfolio_.jpg')}}" class="fancy">
-                <div class="portfolio-media"><img src="{{ asset('assets/frontend/pic/portfolio/1160x570_Portfolio_.jpg')}}" data-at2x="{{ asset('assets/frontend/pic/portfolio/1160x570_Portfolio_.jpg')}}" alt></div></a>
-              <div class="links"><a href="{{ asset('assets/frontend/pic/portfolio/1160x570_Portfolio_.jpg')}}" class="fancy"><i class="fa fa-expand"></i></a></div>
+              <!-- portfolio image--><a href="{{ asset('assets/frontend/pic/portfolio/Gallery_.jpg')}}" class="fancy">
+                <div class="portfolio-media"><img src="{{ asset('assets/frontend/pic/portfolio/Gallery_.jpg')}}" data-at2x="{{ asset('assets/frontend/pic/portfolio/Gallery_.jpg')}}" alt></div></a>
+              <div class="links"><a href="{{ asset('assets/frontend/pic/portfolio/Gallery_.jpg')}}" class="fancy"><i class="fa fa-expand"></i></a></div>
             </div>
           </div>
           <!-- portfolio item-->
-          <div class="col-md-3 col-sm-6 col-xs-6 all family adventure beach">
+          <div class="col-md-3 col-sm-6 col-xs-6 all adventure">
             <div class="portfolio-item">
-              <!-- portfolio image--><a href="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_2.jpg')}}" class="fancy">
-                <div class="portfolio-media"><img src="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_2.jpg')}}" data-at2x="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_2.jpg')}}" alt></div></a>
-              <div class="links"><a href="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_2.jpg')}}" class="fancy"><i class="fa fa-expand"></i></a></div>
+              <!-- portfolio image--><a href="{{ asset('assets/frontend/pic/portfolio/Gallery_2.jpg')}}" class="fancy">
+                <div class="portfolio-media"><img src="{{ asset('assets/frontend/pic/portfolio/Gallery_2.jpg')}}" data-at2x="{{ asset('assets/frontend/pic/portfolio/Gallery_2.jpg')}}" alt></div></a>
+              <div class="links"><a href="{{ asset('assets/frontend/pic/portfolio/Gallery_2.jpg')}}" class="fancy"><i class="fa fa-expand"></i></a></div>
             </div>
           </div>
           <!-- portfolio item-->
-          <div class="col-md-3 col-sm-6 col-xs-6 all romantic wildlife honeymoon">
+          <div class="col-md-3 col-sm-6 col-xs-6 all adventure">
             <div class="portfolio-item">
-              <!-- portfolio image--><a href="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_3.jpg')}}" class="fancy">
-                <div class="portfolio-media"><img src="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_3.jpg')}}" data-at2x="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_3.jpg')}}" alt></div></a>
-              <div class="links"><a href="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_3.jpg')}}" class="fancy"><i class="fa fa-expand"></i></a></div>
+              <!-- portfolio image--><a href="{{ asset('assets/frontend/pic/portfolio/Gallery_3.jpg')}}" class="fancy">
+                <div class="portfolio-media"><img src="{{ asset('assets/frontend/pic/portfolio/Gallery_3.jpg')}}" data-at2x="{{ asset('assets/frontend/pic/portfolio/Gallery_3.jpg')}}" alt></div></a>
+              <div class="links"><a href="{{ asset('assets/frontend/pic/portfolio/Gallery_3.jpg')}}" class="fancy"><i class="fa fa-expand"></i></a></div>
             </div>
           </div>
           <!-- portfolio item-->
-          <div class="col-md-3 col-sm-6 col-xs-6 all family romantic beach">
+          <div class="col-md-3 col-sm-6 col-xs-6 all adventure">
             <div class="portfolio-item">
-              <!-- portfolio image--><a href="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_4.jpg')}}" class="fancy">
-                <div class="portfolio-media"><img src="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_4.jpg')}}" data-at2x="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_4.jpg')}}" alt></div></a>
-              <div class="links"><a href="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_4.jpg')}}" class="fancy"><i class="fa fa-expand"></i></a></div>
+              <!-- portfolio image--><a href="{{ asset('assets/frontend/pic/portfolio/Gallery_4.jpg')}}" class="fancy">
+                <div class="portfolio-media"><img src="{{ asset('assets/frontend/pic/portfolio/Gallery_4.jpg')}}" data-at2x="{{ asset('assets/frontend/pic/portfolio/Gallery_4.jpg')}}" alt></div></a>
+              <div class="links"><a href="{{ asset('assets/frontend/pic/portfolio/Gallery_4.jpg')}}" class="fancy"><i class="fa fa-expand"></i></a></div>
             </div>
           </div>
           <!-- portfolio item-->
-          <div class="col-md-3 col-sm-6 col-xs-6 all adventure wildlife beach">
-            <div class="portfolio-item">
-              <!-- portfolio image--><a href="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_5.jpg')}}" class="fancy">
-                <div class="portfolio-media"><img src="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_5.jpg')}}" data-at2x="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_5.jpg')}}" alt></div></a>
-              <div class="links"><a href="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_5.jpg')}}" class="fancy"><i class="fa fa-expand"></i></a></div>
-            </div>
-          </div>--}}
-          <!-- portfolio item-->
-          <div class="col-md-4 col-sm-6 col-xs-6 all family wildlife honeymoon">
-            <div class="portfolio-item">
-              <!-- portfolio image--><a href="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_6.jpg')}}" class="fancy">
-                <div class="portfolio-media"><img src="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_6.jpg')}}" data-at2x="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_6.jpg')}}" alt></div></a>
-              <div class="links"><a href="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_6.jpg')}}" class="fancy"><i class="fa fa-expand"></i></a></div>
-            </div>
-          </div>
-          <!-- portfolio item-->
-          {{-- <div class="col-md-3 col-sm-6 col-xs-6 all adventure romantic beach">
-            <div class="portfolio-item">
-              <!-- portfolio image--><a href="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_7.jpg')}}" class="fancy">
-                <div class="portfolio-media"><img src="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_7.jpg')}}" data-at2x="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_7.jpg')}}" alt></div></a>
-              <div class="links"><a href="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_7.jpg')}}" class="fancy"><i class="fa fa-expand"></i></a></div>
-            </div>
-          </div>--}}
-          <!-- portfolio item-->
-          <div class="col-md-4 col-sm-6 col-xs-6 all family wildlife honeymoon">
-            <div class="portfolio-item">
-              <!-- portfolio image--><a href="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_8.jpg')}}" class="fancy">
-                <div class="portfolio-media"><img src="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_8.jpg')}}" data-at2x="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_8.jpg')}}" alt></div></a>
-              <div class="links"><a href="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_8.jpg')}}" class="fancy"><i class="fa fa-expand"></i></a></div>
-            </div>
-          </div>
-          <!-- portfolio item-->
-          <div class="col-md-4 col-sm-6 col-xs-6 all adventure romantic wildlife">
-            <div class="portfolio-item">
-              <!-- portfolio image--><a href="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_.jpg')}}" class="fancy">
-                <div class="portfolio-media"><img src="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_.jpg')}}" data-at2x="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_.jpg')}}" alt></div></a>
-              <div class="links"><a href="{{ asset('assets/frontend/pic/portfolio/570x570_Portfolio_.jpg')}}" class="fancy"><i class="fa fa-expand"></i></a></div>
-            </div>
-          </div>
-          <!-- portfolio item-->
-          <div class="col-md-6 col-sm-12 col-xs-12 all family romantic beach honeymoon">
+          <div class="col-md-6 col-sm-12 col-xs-12 all adventure">
             <div class="portfolio-item big">
-              <!-- portfolio image--><a href="{{ asset('assets/frontend/pic/portfolio/1160x570_Portfolio_2.jpg')}}" class="fancy">
-                <div class="portfolio-media"><img src="{{ asset('assets/frontend/pic/portfolio/1160x570_Portfolio_2.jpg')}}" data-at2x="{{ asset('assets/frontend/pic/portfolio/1160x570_Portfolio_2.jpg')}}" alt></div></a>
-              <div class="links"><a href="{{ asset('assets/frontend/pic/portfolio/1160x570_Portfolio_2.jpg')}}" class="fancy"><i class="fa fa-expand"></i></a></div>
+              <!-- portfolio image--><a href="{{ asset('assets/frontend/pic/portfolio/Gallery_5.jpg')}}" class="fancy">
+                <div class="portfolio-media"><img src="{{ asset('assets/frontend/pic/portfolio/Gallery_5.jpg')}}" data-at2x="{{ asset('assets/frontend/pic/portfolio/Gallery_5.jpg')}}" alt></div></a>
+              <div class="links"><a href="{{ asset('assets/frontend/pic/portfolio/Gallery_5.jpg')}}" class="fancy"><i class="fa fa-expand"></i></a></div>
+            </div>
+          </div>
+          <!-- portfolio item-->
+          <div class="col-md-3 col-sm-6 col-xs-6 all adventure">
+            <div class="portfolio-item">
+              <!-- portfolio image--><a href="{{ asset('assets/frontend/pic/portfolio/Gallery_6.jpg')}}" class="fancy">
+                <div class="portfolio-media"><img src="{{ asset('assets/frontend/pic/portfolio/Gallery_6.jpg')}}" data-at2x="{{ asset('assets/frontend/pic/portfolio/Gallery_6.jpg')}}" alt></div></a>
+              <div class="links"><a href="{{ asset('assets/frontend/pic/portfolio/Gallery_6.jpg')}}" class="fancy"><i class="fa fa-expand"></i></a></div>
+            </div>
+          </div>
+          <!-- portfolio item-->
+          <div class="col-md-3 col-sm-6 col-xs-6 all adventure">
+            <div class="portfolio-item">
+              <!-- portfolio image--><a href="{{ asset('assets/frontend/pic/portfolio/Gallery_7.jpg')}}" class="fancy">
+                <div class="portfolio-media"><img src="{{ asset('assets/frontend/pic/portfolio/Gallery_7.jpg')}}" data-at2x="{{ asset('assets/frontend/pic/portfolio/Gallery_7.jpg')}}" alt></div></a>
+              <div class="links"><a href="{{ asset('assets/frontend/pic/portfolio/Gallery_7.jpg')}}" class="fancy"><i class="fa fa-expand"></i></a></div>
+            </div>
+          </div>
+          <!-- portfolio item-->
+          <div class="col-md-3 col-sm-6 col-xs-6 all adventure">
+            <div class="portfolio-item">
+              <!-- portfolio image--><a href="{{ asset('assets/frontend/pic/portfolio/Gallery_8.jpg')}}" class="fancy">
+                <div class="portfolio-media"><img src="{{ asset('assets/frontend/pic/portfolio/Gallery_8.jpg')}}" data-at2x="{{ asset('assets/frontend/pic/portfolio/Gallery_8.jpg')}}" alt></div></a>
+              <div class="links"><a href="{{ asset('assets/frontend/pic/portfolio/Gallery_8.jpg')}}" class="fancy"><i class="fa fa-expand"></i></a></div>
+            </div>
+          </div>
+          <!-- portfolio item-->
+          <div class="col-md-3 col-sm-6 col-xs-6 all adventure">
+            <div class="portfolio-item">
+              <!-- portfolio image--><a href="{{ asset('assets/frontend/pic/portfolio/Gallery_10.jpg')}}" class="fancy">
+                <div class="portfolio-media"><img src="{{ asset('assets/frontend/pic/portfolio/Gallery_10.jpg')}}" data-at2x="{{ asset('assets/frontend/pic/portfolio/Gallery_10.jpg')}}" alt></div></a>
+              <div class="links"><a href="{{ asset('assets/frontend/pic/portfolio/Gallery_10.jpg')}}" class="fancy"><i class="fa fa-expand"></i></a></div>
+            </div>
+          </div>
+            <!-- portfolio item-->
+          <div class="col-md-3 col-sm-6 col-xs-6 all adventure">
+            <div class="portfolio-item">
+              <!-- portfolio image--><a href="{{ asset('assets/frontend/pic/portfolio/Gallery_11.jpg')}}" class="fancy">
+                <div class="portfolio-media"><img src="{{ asset('assets/frontend/pic/portfolio/Gallery_11.jpg')}}" data-at2x="{{ asset('assets/frontend/pic/portfolio/Gallery_11.jpg')}}" alt></div></a>
+              <div class="links"><a href="{{ asset('assets/frontend/pic/portfolio/Gallery_11.jpg')}}" class="fancy"><i class="fa fa-expand"></i></a></div>
+            </div>
+          </div>
+            <!-- portfolio item-->
+          <div class="col-md-3 col-sm-6 col-xs-6 all">
+            <div class="portfolio-item">
+              <!-- portfolio image--><a href="{{ asset('assets/frontend/pic/portfolio/Gallery_12.jpg')}}" class="fancy">
+                <div class="portfolio-media"><img src="{{ asset('assets/frontend/pic/portfolio/Gallery_12.jpg')}}" data-at2x="{{ asset('assets/frontend/pic/portfolio/Gallery_12.jpg')}}" alt></div></a>
+              <div class="links"><a href="{{ asset('assets/frontend/pic/portfolio/Gallery_12.jpg')}}" class="fancy"><i class="fa fa-expand"></i></a></div>
+            </div>
+          </div>
+          <!-- portfolio item-->
+          <div class="col-md-6 col-sm-12 col-xs-12 all adventure">
+            <div class="portfolio-item big">
+              <!-- portfolio image--><a href="{{ asset('assets/frontend/pic/portfolio/Gallery_9.jpg')}}" class="fancy">
+                <div class="portfolio-media"><img src="{{ asset('assets/frontend/pic/portfolio/Gallery_9.jpg')}}" data-at2x="{{ asset('assets/frontend/pic/portfolio/Gallery_9.jpg')}}" alt></div></a>
+              <div class="links"><a href="{{ asset('assets/frontend/pic/portfolio/Gallery_9.jpg')}}" class="fancy"><i class="fa fa-expand"></i></a></div>
+            </div>
+          </div>
+            <!-- portfolio item-->
+          <div class="col-md-6 col-sm-12 col-xs-12 all">
+            <div class="portfolio-item big">
+              <!-- portfolio image--><a href="{{ asset('assets/frontend/pic/portfolio/Gallery_13.jpg')}}" class="fancy">
+                <div class="portfolio-media"><img src="{{ asset('assets/frontend/pic/portfolio/Gallery_13.jpg')}}" data-at2x="{{ asset('assets/frontend/pic/portfolio/Gallery_13.jpg')}}" alt></div></a>
+              <div class="links"><a href="{{ asset('assets/frontend/pic/portfolio/Gallery_13.jpg')}}" class="fancy"><i class="fa fa-expand"></i></a></div>
+            </div>
+          </div>
+            <!-- portfolio item-->
+          <div class="col-md-6 col-sm-12 col-xs-12 all">
+            <div class="portfolio-item big">
+              <!-- portfolio image--><a href="{{ asset('assets/frontend/pic/portfolio/Gallery_14.jpg')}}" class="fancy">
+                <div class="portfolio-media"><img src="{{ asset('assets/frontend/pic/portfolio/Gallery_14.jpg')}}" data-at2x="{{ asset('assets/frontend/pic/portfolio/Gallery_14.jpg')}}" alt></div></a>
+              <div class="links"><a href="{{ asset('assets/frontend/pic/portfolio/Gallery_14.jpg')}}" class="fancy"><i class="fa fa-expand"></i></a></div>
             </div>
           </div>
         </div>
